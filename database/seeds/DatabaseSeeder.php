be850fe08d1bbadaf66d5d92fe399d564822ce1c
curso laravel free youtube <maikol.salas.m@gmail.com>
2020-04-01 16:36:26 -0300

    primer commit

---
+++ database/seeds/DatabaseSeeder.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $this->truncateTablas([
+            'rol'
+        ]);
+        $this->call(TablaRolSeeder::class);
+    }
+    
+    
+
+    protected function truncateTablas(array $tablas)
+    {
+        DB::statement('SET FOREIGN_KEY_CHECKS = 0;');
+        foreach($tablas as $tabla){
+            DB::table($tabla)->truncate();
+        }
+    
+        DB::statement('SET FOREIGN_KEY_CHECKS = 1;');
+        
+    }
+
+}
+
+ 
---
+++ database/seeds/TablaRolSeeder.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class TablaRolSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {p
+        $rols=[
+            'administrador',
+            'editor',
+            'supervisor'
+        ];
+        foreach($rols as $key =>$value)
+        {
+            DB::table('rol')->insert([ 
+                'nombre'=>$value
+            ]);
+
+        }
+    }
+}
+
